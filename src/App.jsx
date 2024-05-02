a9533178e6f33218e1987beaec87f4c5d89eea88
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-05-01 21:21:01 -0700

    transition is smooth, image loads with arrow clicking

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ function App() {
   };
 
   return (
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+    <div className='max-w-full h-[780px] w-screen m-auto py-16 px-4 relative group border-2 border-red-800'>
       <div 
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        className='transition-all w-full h-full bg-center bg-cover duration-500'
         ></div>
       {/* left arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl bg-black/20 text-white cursor-pointer'>
