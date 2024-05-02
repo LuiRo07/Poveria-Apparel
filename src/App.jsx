eb09671391762282dc1521d81e130ea36032f37a
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-05-01 19:51:51 -0700

    project works with tailwind installed, react + vite, and react-icons library installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1502163140606-888448ae8cfe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }, 
+    {
+      url: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+  ]
 
   return (
-    <>
-    </>
+    <div>
+      <BsChevronCompactLeft />
+      <BsChevronCompactRight />
+    </div>
   )
 }
 
