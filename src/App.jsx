eb38aaa2ebe55d60da79175c4e20b0e59f46e31c
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-05-02 19:30:43 -0700

    images carousel slide on autoplay, and different headers render for different slides

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,12 @@ function App() {
     }, 
     {
       url: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    },
-    {
-      url: 'https://plus.unsplash.com/premium_photo-1682096572836-9f77d9b32c25?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
   ]
 
   // useStates
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [autPlay, setAutoPlay] = useState(null)
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -34,14 +32,39 @@ function App() {
   };
 
   // autoplay logic
+  useEffect(() => {
+    setTimeout(() => {
+      nextSlide();
+    }, 2500)
+  })
+
+// header component
+function Header({ currentIndex }) {
+  if (currentIndex === 1) {
+    return <h1 className="basis-2/4 text-5xl">Exclusive Cool Summer</h1>
+  } else {
+    return <h1 className="basis-2/4 text-5xl">Exclusive Spring Collection</h1>
+  }
+}
   
 
   return (
-    <div className='flex max-w-full w-screen h-screen relative group'>
+    <div className='flex max-w-full w-screen h-[80vh] relative group'>
       <div 
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='transition-all w-full h-full bg-center bg-cover duration-500'
-        ></div>
+        >
+          <div className="absolute left-[10%] top-[35%]">
+            <h3 className="basis-1/3">Best Relaxing Collection</h3>
+            <div className="my-8">
+              <Header 
+                currentIndex={currentIndex}
+              />
+              <h1 className="text-5xl">Collection</h1>  
+            </div>
+            <button className="rounded-full bg-slate-900 text-slate-300 px-7 py-2 basis-1/3 text-sm">VIEW MORE</button>  
+          </div> 
+        </div>
       {/* left arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft size={30} onClick={prevSlide} />
